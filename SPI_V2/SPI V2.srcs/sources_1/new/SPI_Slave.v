4f0de17d713f56cfd3d2d898b5ea37af04e3387d
andreipopa02 <andrei.popa.020710@gmail.com>
2024-07-21 18:33:25 +0300

    prima versiune funcionala - master

---
+++ SPI_V2/SPI V2.srcs/sources_1/new/SPI_Slave.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 07/18/2024 12:02:54 PM
+// Design Name: 
+// Module Name: SPI_Slave
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module SPI_Slave(
+
+    );
+endmodule
